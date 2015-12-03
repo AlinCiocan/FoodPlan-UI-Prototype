781eddfa1def467fc39a28689c4316e4f32f35e6
Alin Ciocan <alin_c9@yahoo.com>
2015-12-03 18:27:18 +0200

    populated the model with some basic info

--- version3/js/app.js
+++ version3/js/app.js
@@ -1,5 +1,12 @@
 /* global angular*/
 (function () {
 	var app = angular.module("foodplan", []);
-
+	app.controller("WeekController", ["$scope", function ($scope) {
+		// populate the model
+		$scope.week = window.week;
+		
+		
+		
+		
+	}]);
 })();
---
+++ version3/js/model.js
@@ -0,0 +1,34 @@
+(function () {
+	function createCategory(categoryName, items) {
+		return {
+			name: categoryName,
+			items: items
+		};
+	}
+
+	function createDay(dayName, categories) {
+		return {
+			title: dayName,
+			categories: categories
+		};
+	}
+
+	var breakfast = createCategory("Breakfast", ["eggs", "juice"]);
+	var lunch = createCategory("Lunch", ["peas soup", "polenta", "tomatoes"]);
+	var dinner = createCategory("Dinner", ["fruit salad", "yogurt"]);
+	var cooking = createCategory("Cooking", ["beans soup", "rice with mushrooms"]);
+	var shopping = createCategory("Shopping", ["oranges 1kg", "one bottle of milk", "bannanas"]);
+
+	var week = [
+		createDay("Monday", [breakfast, lunch, dinner]),
+		createDay("Tuesday", [lunch, shopping, dinner, cooking]),
+		createDay("Wednesday", [breakfast, lunch, shopping, cooking]),
+		createDay("Thursday", [breakfast, lunch, shopping, dinner, cooking]),
+		createDay("Friday", [breakfast, lunch, dinner]),
+		createDay("Saturday", [lunch, shopping, dinner, cooking]),
+		createDay("Sunday", [breakfast, lunch, shopping, cooking]),
+	];
+	
+	
+	window.week = week;
+}) ();
